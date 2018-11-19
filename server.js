fea27baa7e231d3bbd010c5a1b40cfe9730d22e4
Kavita Sharma <kavitasharma@Kavitas-MacBook-Air.local>
2018-11-19 20:06:20 +0530

    db creentails changed

--- server.js
+++ server.js
@@ -2,30 +2,44 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-var mysql = require('mysql');
+//var mysql = require('mysql');
+const pg = require('pg');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Technical",
-  database : 'login'
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Technical",
+//   database : 'login'
+// });
+//
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+const config = {
+    host: 'ec2-174-129-236-147.compute-1.amazonaws.com',
+    user: 'hboqxxybclwxds',
+    password: 'cc8c194ef44da7690155bb24bb809c91fa756fc71bc351156397b77a8c71af27',
+    database: 'dadcg8tiaucmg8',
+    port: 5432,
+    ssl:true,
+};
+const client = new pg.Client(config);
+client.connect(err => {
+    if (err) throw err;
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.use(express.static(path.resolve(__dirname, './build')));
 app.use(bodyParser.json());
 
 
 app.get('/getUsers',function(request,response) {
-  connection.query("select * from users", function (error, results, fields) {
+  client.query("select * from users", function (error, results) {
     if (error) throw error;
     if(results) {
-       response.json({"status":"success","users":results});
+       response.json({"status":"success","users":results.rows});
     }
   });
 });
@@ -33,10 +47,10 @@ app.get('/getUsers',function(request,response) {
 app.post('/updateUserStatus', function(request,response) {
   const status = request.body.status;
   const userid = Number(request.body.userid);
-  connection.query("update users set status='"+status+"' where id="+userid+"", function (error, results, fields) {
+  client.query("update users set status='"+status+"' where id="+userid+"", function (error, results) {
     if (error) throw error;
     if(results) {
-       response.json({"status":"success","message":results});
+       response.json({"status":"success","message":results.rows});
     }
   });
 });
@@ -50,7 +64,7 @@ app.post('/addUser', function(request, response) {
     if (error) throw error;
     if(results) {
       if(results.length>0){
-      response.json({"status":"success","userdata":results[0]});
+      response.json({"status":"success","userdata":results.rows[0]});
     }
     else {
       connection.query("insert into users(first_name,last_name,email,status) VALUES('"+first_name+"','"+last_name+"','"+email+"','Active')", function (error, results, fields) {
